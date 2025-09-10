87e19bae2d065a1db6b8b822cf60a97b26db46e6
DanYL426 <leedani@bc.edu>
2025-09-09 22:39:58 -0400

    Submitting PS-1

--- ps1-part3.py
+++ ps1-part3.py
@@ -1,1 +1,14 @@
-
+def heart_rate(age,goal):
+    max_HR = 220-age
+    print(f"Your maximum heart rate is: {max_HR}")
+    if goal == "S":
+        minimum = float(max_HR*0.8)
+        maximum = float(max_HR)
+    if goal == "E":
+        minimum = float(max_HR*0.6)
+        maximum = float(max_HR*0.8)
+    print(f"Your target heart rate is between {minimum} and {maximum}")
+    
+user_age = int(input("What is your age? "))
+user_goal = input("Is your goal speed (S) or endurance (E)? ")
+heart_rate(user_age,user_goal)
--- ps1-part4.py
+++ ps1-part4.py
@@ -1,1 +1,17 @@
+priors = int(input("Prior arrests: "))
+priors_local_ord = input("Prior arrests for local ordinance (Y/N): ")
+release_age = int(input("Age at release: "))
 
+point_total = 0
+if priors >= 2:
+    point_total += 1
+if priors >= 5:
+    point_total += 1
+if priors_local_ord == "Y":
+    point_total += 1
+if release_age >= 18 and release_age <= 24:
+    point_total += 1
+if release_age >= 40:
+    point_total -= 1
+    
+print(f"The recidivism risk score is {point_total}")
